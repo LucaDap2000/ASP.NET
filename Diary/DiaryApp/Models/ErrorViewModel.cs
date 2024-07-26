73f6d6f05f5f09f40a5f7704446c83824c60d3b9
LucaDap2000 <155812004+LucaDap2000@users.noreply.github.com>
2024-07-26 13:38:37 +0100

    Delete Diary directory

--- Diary/DiaryApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DiaryApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
