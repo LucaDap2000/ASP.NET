a0f71e3d6bf5cfae1bff26eae4055f45d2d1c809
LucaDap2000 <luca.daprano@outlook.com>
2024-08-16 10:34:34 +0100

    MVC Basics Upload

---
+++ MVCBasics/MVCBasics/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBasics.Models;
+
+namespace MVCBasics.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            // return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
---
+++ MVCBasics/MVCBasics/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCBasics.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
